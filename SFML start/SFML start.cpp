9a7eb25ce79f2859213b5362c32bbc665976da3c
JuniorX22 <guyjunior.ooms@atheneumsinttruiden>
2025-05-18 14:46:00 +0200

    Game starting

--- SFML start/SFML start.cpp
+++ SFML start/SFML start.cpp
@@ -2,137 +2,67 @@
 //
 
 #include <SFML/Graphics.hpp>
-#include <vector>
 #include <iostream>
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(512, 512), "SFML Tutorial", sf::Style::Close | sf::Style::Resize);
+	sf::RectangleShape player(sf::Vector2f(100.0f, 100.0f));
+	player.setFillColor(sf::Color::Red);
+	player.setOrigin(50.0f, 50.0f);
 
-// Window size
-const int WIDTH = 800;
-const int HEIGHT = 600;
+	while (window.isOpen())
+	{
+		sf::Event evnt;
+		while (window.pollEvent(evnt))
+		{
+			switch (evnt.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			case sf::Event::Resized:
+				//std::cout << "New window width: " << evnt.size.width <<" New window height: " << evnt.size.height << std::endl;
+				printf("New window width: %i New window height: %i\n", evnt.size.width, evnt.size.height);
+				break;
+			case sf::Event::TextEntered:
+				if (evnt.text.unicode < 128)
+				{
+					printf("%c", evnt.text.unicode); 
+				}
+				
+			}
 
-// Enemy class
-class Enemy {
-public:
-    sf::RectangleShape shape;
-    float speed = 2.0f;
 
-    Enemy(float x, float y) {
-        shape.setSize(sf::Vector2f(40, 40));
-        shape.setFillColor(sf::Color::Red);
-        shape.setPosition(x, y);
-    }
+			/*
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q)) 
+			{
+				player.move(-0.1f, 0.0f);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+			{
+				player.move(0.1f, 0.0f);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Z))
+			{
+				player.move(0.0f, -0.1f);
+			}
+			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+			{
+				player.move(-0.0f, 0.1f);
+			}
+			*/
+			if (sf::Mouse::isButtonPressed(sf::Mouse::Left))
+			{
+				sf::Vector2i mousePos = sf::Mouse::getPosition(window);
+				player.setPosition((float)mousePos.x, (float)(mousePos.y));
+			}
 
-    void move() {
-        shape.move(speed, 0);
-    }
-
-    bool reachedEnd() {
-        return shape.getPosition().x >= WIDTH;
-    }
-};
-
-// Bullet class
-class Bullet {
-public:
-    sf::CircleShape shape;
-    float speed = 5.0f;
-
-    Bullet(float x, float y) {
-        shape.setRadius(5);
-        shape.setFillColor(sf::Color::White);
-        shape.setPosition(x, y);
-    }
-
-    void move() {
-        shape.move(speed, 0);
-    }
-};
-
-int main() {
-    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Simple Tower Defense");
-    sf::RectangleShape tower(sf::Vector2f(50, 100));
-    tower.setFillColor(sf::Color::Blue);
-    tower.setPosition(50, HEIGHT / 2 - 50);
-
-    std::vector<Enemy> enemies;
-    std::vector<Bullet> bullets;
-
-    sf::Clock enemySpawnClock;
-    sf::Clock bulletClock;
-
-    bool gameOver = false;
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        if (!gameOver) {
-            // Spawn enemies every 2 seconds
-            if (enemySpawnClock.getElapsedTime().asSeconds() > 2) {
-                enemies.push_back(Enemy(0, rand() % (HEIGHT - 40)));
-                enemySpawnClock.restart();
-            }
-
-            // Shoot bullets every 0.5 seconds
-            if (bulletClock.getElapsedTime().asSeconds() > 0.5) {
-                bullets.push_back(Bullet(100, HEIGHT / 2));
-                bulletClock.restart();
-            }
-
-            // Move bullets
-            for (auto& bullet : bullets) bullet.move();
-
-            // Move enemies
-            for (auto& enemy : enemies) enemy.move();
-
-            // Check for collisions
-            for (auto it = bullets.begin(); it != bullets.end();) {
-                bool bulletDeleted = false;
-                for (auto jt = enemies.begin(); jt != enemies.end();) {
-                    if (it->shape.getGlobalBounds().intersects(jt->shape.getGlobalBounds())) {
-                        it = bullets.erase(it);
-                        jt = enemies.erase(jt);
-                        bulletDeleted = true;
-                        break;
-                    }
-                    else {
-                        ++jt;
-                    }
-                }
-                if (!bulletDeleted) ++it;
-            }
-
-            // Check if an enemy reaches the right side
-            for (auto& enemy : enemies) {
-                if (enemy.reachedEnd()) {
-                    gameOver = true;
-                    break;
-                }
-            }
-        }
-
-        window.clear();
-
-        if (gameOver) {
-            sf::Font font;
-            font.loadFromFile("arial.ttf");
-            sf::Text text("YOU DIED", font, 50);
-            text.setFillColor(sf::Color::Red);
-            text.setPosition(WIDTH / 2 - 100, HEIGHT / 2);
-            window.draw(text);
-        }
-        else {
-            window.draw(tower);
-            for (auto& enemy : enemies) window.draw(enemy.shape);
-            for (auto& bullet : bullets) window.draw(bullet.shape);
-        }
-
-        window.display();
-    }
-
-    return 0;
+			window.clear();
+			window.draw(player);
+			window.display();
+		}
+	}
+	return 0;
 }
 
 
